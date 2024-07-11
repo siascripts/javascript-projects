fc4580d5ec5c7e79cdc2d0c32e7fdc9b41fc8324
titiksha-coder <sianarang22@gmail.com>
2024-07-11 17:53:23 +0530

    Added = operation and fixed Run Time errors.

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,7 +1,7 @@
 const display = document.getElementById("display");
 let displayValue = "";
 let currentValue = "";
-let operator = "";
+let lastOperator = "";
 let result = 0;
 let firstOperation = true;
 
@@ -12,51 +12,66 @@ buttons.forEach((button) => {
     checkValue(value);
   });
 });
+const handleNumber = (value) => {
+  displayValue = displayValue + value;
+  display.innerText = displayValue;
+  currentValue = currentValue + value;
+};
+
+const handleOperation = (op) => {
+  if (parseFloat(currentValue)) {
+    switch (op) {
+      case "+":
+        result = result + parseFloat(currentValue);
+        display.innerText = result;
+        currentValue = displayValue = "";
+        break;
+      case "-":
+        if (currentValue) {
+          result = result - parseFloat(currentValue);
+          display.innerText = result;
+          currentValue = displayValue = "";
+        }
+        break;
+      case "/":
+        result = result / parseFloat(currentValue);
+        display.innerText = result;
+        currentValue = displayValue = "";
+        break;
+      case "*":
+        result = result * parseFloat(currentValue);
+        display.innerText = result;
+        currentValue = displayValue = "";
+        break;
+      case "=":
+        handleOperation(lastOperator);
+        break;
+    }
+  }
+};
+
+const handleOperator = (op) => {
+  debugger;
+
+  if (firstOperation && parseFloat(currentValue)) {
+    if (op !== "=") {
+      lastOperator = op;
+    }
+    result = parseFloat(currentValue);
+    display.innerText = result;
+    firstOperation = false;
+    currentValue = displayValue = "";
+  } else {
+    handleOperation(lastOperator);
+    if (op !== "=") {
+      lastOperator = op;
+    }
+  }
+};
+
 const checkValue = (value) => {
   debugger;
-  if (!isNaN(value)) {
+  if (!isNaN(value) || value === ".") {
     handleNumber(value);
   } else handleOperator(value);
-
-  const handleNumber = (value) => {
-    displayValue = displayValue + value;
-    display.innerText = displayValue;
-    currentValue = currentValue + value;
-  };
-
-  const handleOperator = (op) => {
-    operator = "op";
-    if (firstOperation) {
-      result = parseInt(currentValue);
-      display.innerText = result;
-      console.log("result 55", result);
-      firstOperation = false;
-      currentValue = displayValue = "";
-    } else {
-      switch (op) {
-        case "+":
-          result = result + parseInt(currentValue);
-          display.innerText = result;
-          currentValue = displayValue = "";
-          break;
-        case "-":
-          if (currentValue) {
-            result = result - parseInt(currentValue);
-            display.innerText = result;
-            currentValue = displayValue = "";
-          }
-          break;
-        case "/":
-          result = result / parseInt(currentValue);
-          display.innerText = result;
-          currentValue = displayValue = "";
-          break;
-        case "*":
-          result = result * parseInt(currentValue);
-          display.innerText = result;
-          currentValue = displayValue = "";
-          break;
-      }
-    }
-  };
 };
